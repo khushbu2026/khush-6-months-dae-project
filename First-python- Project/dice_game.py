ef172847d713a168dcf50df27a5d49a0c07fada4
khushbu2026 <khushbu.jalan14@gmail.com>
2026-02-10 12:08:28 -0500

    create course folder structure

---
+++ First-python- Project/dice_game.py
@@ -0,0 +1,16 @@
+#print a wlcome message
+print("welcome to our game")
+
+#asking the user to enter a number and start the game
+user_roll_resonse_string=input("press 1 to roll a die ")
+
+
+converting user roll response that was a string
+user_roll_response=int("user roll response")
+
+while user_roll_response != 1:
+    userrollresponsestring=input("please rememember to press 1 to roll a die")
+    user_roll_resonse_int=int (user_roll_resonse string)
+
+# print(" the user pressed 1 ")
+
