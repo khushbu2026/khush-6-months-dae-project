f34cdd7af7e2f1a8e9bc1cd12bd4fb969ddd9b10
khushbu2026 <Khushbu.jalan14@gmail.com>
2026-03-05 13:09:34 -0500

    first

--- python_1/project.py
+++ python_1/project.py
@@ -5,16 +5,15 @@ print("welcome to ECOSTYLE sustainable fashion")
 print("select one from each catergory")
 
 #ask the user to select from the option 1#occasion(party,casual.wedding)
-occasion=input("select occasion( 'Wedding', 'Party', 'Casual':")
+occasion=input("please enter occasion( 'Wedding', 'Party', 'Casual':")
 
 #if the user is not selecting the given options in occasion show an error
 if  occasion != 'Wedding' and occasion != 'party' and occasion != 'casual':
     print("Error!! please enter Wedding, Party or Casual")
-    occasion=input("Select occasion Wedding, Party or casual: ")
-   
+
     different_event = input("Did user enter different event? (Yes/No): ")
 
-#if the user is selecting NO, proceed to to the next choice
+#if the occassion is correct,continue
 #select weather (hot,mild, cold)
 else:
      weather = input("Please enter the weather (Hot, Cold, Mild): ")
@@ -23,23 +22,22 @@ else:
 
      if weather != 'hot'and weather !=  'cold'and weather != 'mild':
       print ("Error!! please enter hot,coldor mild") 
-     weather = input("Please enter the weather (Hot, Cold, Mild): ")
-
-#Check the user input
+    #Check the user input
      different_weather = input("Did user enter different weather? (Yes/No): ")
 
-     #if the user is selecting NO, proceed to to the next choice
+     #if the weather is correct,continue
      fabric = input("Please enter the fabric (Cotton, Silk, Jute): ")
 
   #if the user is not selecting the given option in fabric show an error
 if fabric !='cotton' and fabric !='silk'and fabric !='jute':
  print("Error!! please enter cotton,silk, jute")
+different_fabric = input("did user enter something different? YES/NO")
 
-else:
-    print("Processing information of user selection...")
-    print("Showing suggestions...")
+  
+print("Processing information of user selection...")
+print("Showing suggestions...")
 
-    continue_choice = input("Would you like to continue getting more suggestions? (Yes/No): ")
+continue_choice = input("Would you like to continue getting more suggestions? (Yes/No): ")
 
-if continue_choice == "No":
+if  continue_choice == "No":
     print("Thank you. See you next time")
